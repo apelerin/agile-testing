0b42bc896295a696e2524ee7a8aaecc8a02a2f9e
Axel Pelerin <arcanynka@gmail.com>
2021-01-07 23:15:13 +0100

    Update shell script to compile and test acceptances

--- acceptance-tests/compileAcceptanceTests.sh
+++ acceptance-tests/compileAcceptanceTests.sh
@@ -3,9 +3,11 @@ cd src/ &&
 javac -d ../bin/ test/acceptance/Homepage*.java &&
 javac -d ../bin/ test/acceptance/ConfigModelS*.java &&
 javac -d ../bin/ test/acceptance/Evenement*.java &&
+javac -d ../bin/ test/acceptance/CaracteristicsTab*.java &&
 
 cd ../bin &&
 
 java org.junit.runner.JUnitCore test.acceptance.HomepageTest &&
 java org.junit.runner.JUnitCore test.acceptance.ConfigModelSTest &&
-java org.junit.runner.JUnitCore test.acceptance.EvenementTest
+java org.junit.runner.JUnitCore test.acceptance.EvenementTest &&
+java org.junit.runner.JUnitCore test.acceptance.CaracteristicsTabTest
